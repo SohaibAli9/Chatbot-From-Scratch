b2319cf911014b3166fe0cca030bb4fcdec6e5e7
SohaibAli9 <sohaibali999@gmail.com>
2024-05-18 03:58:15 +0500

    get route

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from chat import get_response
 app = Flask(__name__)
 CORS(app)
 
-# @app.get("/")
-# def index_get():
-#     return render_template("base.html")
+@app.get("/")
+def index_get():
+    return render_template("base.html")
 
 @app.post("/predict")
 def predict():
